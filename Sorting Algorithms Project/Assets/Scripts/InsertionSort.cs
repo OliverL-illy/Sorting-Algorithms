afd01ade915136402f00727be68910d62916207f
OliverL-illy <Oliverlilly@icloud.com>
2025-10-30 16:28:11 +0000

    Update InsertionSort.cs pt3

--- Sorting Algorithms Project/Assets/Scripts/InsertionSort.cs
+++ Sorting Algorithms Project/Assets/Scripts/InsertionSort.cs
@@ -1,22 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InsertionSort : MonoBehaviour
 {
     public void ISort(List<int> valueList)
     {
-        for (i = 0, i < valueList.count, i++)
+        for (int i = 0; i < valueList.Count - 1; i++)
         {
             if (valueList[i + 1] < valueList[i])
             {
-                j = i
-                while (valueList[i + 1] < valueList[j)
+                int j = i;
+                while (j >= 0 && valueList[i + 1] < valueList[j])
                 {
-                    int endPos = j + 1
-                    j--
+                    j--;
                 }
-                int value = valueList[i + 1]
-                valueList.RemoveAt(i + 1)
-                valueList.Insert(endPos, value)
+
+                int endPos = j + 1;
+                int value = valueList[i + 1];
+                valueList.RemoveAt(i + 1);
+                valueList.Insert(endPos, value);
             }
         }
     }
