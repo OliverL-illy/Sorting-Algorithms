d114d886c4981462915da89bb69f874df398cd24
OliverL-illy <Oliverlilly@icloud.com>
2025-12-02 23:56:40 +0000

    first try QuickSort.cs

--- Sorting Algorithms Project/Assets/Scripts/QuickSort.cs
+++ Sorting Algorithms Project/Assets/Scripts/QuickSort.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class QuickSort : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public WindowGraph windowGraph;
+    public List<int> valueList;
+
+    void QSort()
     {
-        
+        int startPivot = valueList.Count/2;
+        PivotSubSort(startPivot);
     }
 
-    // Update is called once per frame
-    void Update()
+    void PivotSubSort(int pivot)
     {
-        
-    }
+        for (i = 0; i < pivot; i++)
+        {
+            if (valueList[i] > valueList[pivot])
+            {
+                int temp = valueList[i]
+                valueList.RemoveAt(i)
+                valueList.Insert(pivot + 1, temp)
+            }
+        }
+        for (i = pivot + 1; i < valueList.Count; i++)
+        {
+            if (valueList[i] < valueList[pivot])
+            {
+                int temp = valueList[i]
+                valueList.RemoveAt(i)
+                valueList.Insert(pivot - 1, temp)
+            }
+        }
 }
