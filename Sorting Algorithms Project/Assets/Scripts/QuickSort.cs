cbc858e2da869ba39bfff8e6ad18d971d92399fd
OliverL-illy <Oliverlilly@icloud.com>
2025-12-03 08:28:08 +0000

    quick second Update QuickSort.cs

--- Sorting Algorithms Project/Assets/Scripts/QuickSort.cs
+++ Sorting Algorithms Project/Assets/Scripts/QuickSort.cs
@@ -4,15 +4,22 @@ public class QuickSort : MonoBehaviour
 {
     public WindowGraph windowGraph;
     public List<int> valueList;
+    private int iterations = 1/2;
 
     void QSort()
     {
         int startPivot = valueList.Count/2;
         PivotSubSort(startPivot);
+        int leftPivot = startPivot/2
+        int rightPivot = startPivot + leftPivot
+        iterations = iterations * (1/2)
+        int pivotRange = valueList.Count * iterations
+        PivotSubSort(leftPivot);
+        PivotSubSort(rightPivot);
     }
 
     void PivotSubSort(int pivot)
-    {
+    { //add if pivotRange = 1 or 0
         for (i = 0; i < pivot; i++)
         {
             if (valueList[i] > valueList[pivot])
