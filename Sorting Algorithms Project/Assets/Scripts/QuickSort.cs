77ee9f3da051778ff23b7760ddbf1c658de0e360
OliverL-illy <Oliverlilly@icloud.com>
2025-12-03 21:38:00 +0000

    Had another go at quick sort, recursive func

--- Sorting Algorithms Project/Assets/Scripts/QuickSort.cs
+++ Sorting Algorithms Project/Assets/Scripts/QuickSort.cs
@@ -1,41 +1,64 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class QuickSort : MonoBehaviour
 {
     public WindowGraph windowGraph;
     public List<int> valueList;
-    private int iterations = 1/2;
+    public int arrayCount;
+    private int leftIterations = 2;
+    private int rightIterations = 2;
+    int leftPivot;
+    int rightPivot;
 
     void QSort()
     {
-        int startPivot = valueList.Count/2;
-        PivotSubSort(startPivot);
-        int leftPivot = startPivot/2
-        int rightPivot = startPivot + leftPivot
-        iterations = iterations * (1/2)
-        int pivotRange = valueList.Count * iterations
-        PivotSubSort(leftPivot);
-        PivotSubSort(rightPivot);
+        PivotStart(valueList.Count / 2);
+        windowGraph.ShowGraph(valueList, -1, (int _i) => "" + (_i + 1), (float _f) => "" + Mathf.RoundToInt(_f));
     }
+    void PivotStart(int pivot)
+    {
+        int leftPivotRange = valueList.Count / leftIterations;
 
-    void PivotSubSort(int pivot)
-    { //add if pivotRange = 1 or 0
-        for (i = 0; i < pivot; i++)
+        if (leftPivotRange > 0)
         {
-            if (valueList[i] > valueList[pivot])
+            for (int i = (pivot - (leftPivotRange / 2)); i < pivot; i++)
             {
-                int temp = valueList[i]
-                valueList.RemoveAt(i)
-                valueList.Insert(pivot + 1, temp)
+                if (valueList[i] > valueList[pivot])
+                {
+                    int temp = valueList[i];
+                    valueList.RemoveAt(i);
+                    valueList.Insert(pivot + 1, temp);
+                }
+            }
+
+            if (pivot / 2 > 0)
+            {
+                leftPivot = pivot / 2;
+                leftIterations *= 2;
+                PivotStart(leftPivot);
             }
         }
-        for (i = pivot + 1; i < valueList.Count; i++)
+
+        int rightPivotRange = valueList.Count / rightIterations;
+
+        if (rightPivotRange > 0)
         {
-            if (valueList[i] < valueList[pivot])
+            for (int i = pivot + 1; i < (pivot + (leftPivotRange / 2)); i++)
             {
-                int temp = valueList[i]
-                valueList.RemoveAt(i)
-                valueList.Insert(pivot - 1, temp)
+                if (valueList[i] < valueList[pivot])
+                {
+                    int temp = valueList[i];
+                    valueList.RemoveAt(i);
+                    valueList.Insert(pivot, temp);
+                }
             }
         }
+        if (rightPivotRange / 2 > 0)
+        {
+            rightPivot = pivot + (leftPivotRange / 2);
+            PivotStart(rightPivot);
+            rightIterations *= 2;
+        }
+    }
 }
